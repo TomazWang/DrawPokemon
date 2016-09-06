a4111f870b3e84cc996ed8f4efbd7e0d45f6ad88
Tomaz Wang <TomazWang.tw@gmail.com>
2016-09-06 22:14:44 +0800

    - Count down timer.

--- app/src/main/java/idv/tomazwang/app/drawpokemon/MainActivity.java
+++ app/src/main/java/idv/tomazwang/app/drawpokemon/MainActivity.java
@@ -3,6 +3,8 @@ package idv.tomazwang.app.drawpokemon;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.annotation.IntDef;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
@@ -15,6 +17,7 @@ import idv.tomazwang.app.drawpokemon.view.DrawingView;
 public class MainActivity extends AppCompatActivity {
 
     private static final int COLUMN_SIZE = 4;
+    private static final int GAME_TIME = 30; //seconds.
 
 
     private DrawingView mDrawingView;
@@ -25,6 +28,19 @@ public class MainActivity extends AppCompatActivity {
     private int mSelectedColor = Color.BLACK;
     private ColorPickerDialog mColorPickerDialog;
     private ImageView mColorPickBtn;
+
+    private static final int TIMER_STOP = 0;
+    private static final int TIMER_RUNNING = 1;
+    private static final int TIMER_PAUSE = 2;
+
+    @IntDef({TIMER_STOP,TIMER_RUNNING,TIMER_PAUSE})
+    public @interface TimerState{};
+
+    @TimerState private int mTimerState = TIMER_STOP;
+
+    private long mRemainTime = 0;
+    private CountDownTimer mTimer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +63,97 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
 
 
+        mPokemonPic.setOnClickListener(v -> {
+
+            if(mTimerState == TIMER_STOP){
+                startTimer(GAME_TIME);
+            }else if(mTimerState == TIMER_PAUSE){
+                resumeTimer();
+            }
+
+        });
+
+
     }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        pauseTimer();
+    }
+
+
+
+    private void startGame(){
+
+        // TODO: show pokemon pic
+
+        startTimer(GAME_TIME);
+
+
+    }
+
+
+
+    private void pauseGame(){
+        // TODO: show pause dialog
+    }
+
+
+    private void stopGame(){
+        // TODO: after time's up
+    }
+
+    private void startTimer(int sec) {
+
+        if(mTimer != null){
+            mTimer.cancel();
+        }
+
+        mTimer = new CountDownTimer(sec*1000, 100){
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimerText.setText(""+millisUntilFinished/1000);
+                mRemainTime = millisUntilFinished;
+            }
+
+            @Override
+            public void onFinish() {
+                mTimerText.setText("0");
+
+            }
+
+        };
+
+        mTimer.start();
+        mTimerState = TIMER_RUNNING;
+    }
+
+
+    private void stopTimer(){
+        if(mTimer != null){
+            mTimer.cancel();
+            mTimerText.setText("0");
+            mTimerState = TIMER_STOP;
+        }
+    }
+
+
+    private void pauseTimer(){
+        if(mTimer != null){
+            mTimer.cancel();
+            mTimerState = TIMER_PAUSE;
+        }
+    }
+
+    private void resumeTimer(){
+        startTimer((int) (mRemainTime/1000));
+    }
+
+
+
 
     private void showColorPicker() {
 
@@ -88,33 +194,7 @@ public class MainActivity extends AppCompatActivity {
     protected void setPickerColor(int color) {
 
         mDrawingView.setPaintColor(mSelectedColor);
-
-        if(color == cColor(R.color.paint_red)){
-            mColorPickBtn.setImageDrawable(cDrawable(R.drawable.painter_palette_red));
-        }else if(color == cColor(R.color.paint_orange)){
-            mColorPickBtn.setImageDrawable(cDrawable(R.drawable.painter_palette_orange));
-        }else if(color == cColor(R.color.paint_yellow)){
-            mColorPickBtn.setImageDrawable(cDrawable(R.drawable.painter_palette_yellow));
-        }else if(color == cColor(R.color.paint_green_light)){
-            mColorPickBtn.setImageDrawable(cDrawable(R.drawable.painter_palette_green_l));
-        }else if(color == cColor(R.color.paint_green)){
-            mColorPickBtn.setImageDrawable(cDrawable(R.drawable.painter_palette_green));
-        }else if(color == cColor(R.color.paint_blue)){
-            mColorPickBtn.setImageDrawable(cDrawable(R.drawable.painter_palette_blue));
-        }else if(color == cColor(R.color.paint_blue_light)){
-            mColorPickBtn.setImageDrawable(cDrawable(R.drawable.painter_palette_blue_l));
-        }else if(color == cColor(R.color.paint_purple)){
-            mColorPickBtn.setImageDrawable(cDrawable(R.drawable.painter_palette_purple));
-        }else if(color == cColor(R.color.paint_pink)){
-            mColorPickBtn.setImageDrawable(cDrawable(R.drawable.painter_palette_pink));
-        }else if(color == cColor(R.color.paint_brown)){
-            mColorPickBtn.setImageDrawable(cDrawable(R.drawable.painter_palette_brown));
-        }else if(color == cColor(R.color.paint_gray)){
-            mColorPickBtn.setImageDrawable(cDrawable(R.drawable.painter_palette_gray));
-        }else if(color == cColor(R.color.paint_black)){
-            mColorPickBtn.setImageDrawable(cDrawable(R.drawable.painter_palette_black));
-        }
-
+        mColorPickBtn.setImageDrawable(cDrawable(colorToDrawable(color)));
 
     }
 
@@ -127,6 +207,35 @@ public class MainActivity extends AppCompatActivity {
         return ContextCompat.getDrawable(this, id);
     }
 
+    private int colorToDrawable(int color){
 
+        if(color == cColor(R.color.paint_red)){
+            return R.drawable.painter_palette_red;
+        }else if(color == cColor(R.color.paint_orange)){
+           return R.drawable.painter_palette_orange;
+        }else if(color == cColor(R.color.paint_yellow)){
+            return R.drawable.painter_palette_yellow;
+        }else if(color == cColor(R.color.paint_green_light)){
+            return R.drawable.painter_palette_green_l;
+        }else if(color == cColor(R.color.paint_green)){
+            return R.drawable.painter_palette_green;
+        }else if(color == cColor(R.color.paint_blue)){
+            return R.drawable.painter_palette_blue;
+        }else if(color == cColor(R.color.paint_blue_light)){
+            return R.drawable.painter_palette_blue_l;
+        }else if(color == cColor(R.color.paint_purple)){
+            return R.drawable.painter_palette_purple;
+        }else if(color == cColor(R.color.paint_pink)){
+            return R.drawable.painter_palette_pink;
+        }else if(color == cColor(R.color.paint_brown)){
+            return R.drawable.painter_palette_brown;
+        }else if(color == cColor(R.color.paint_gray)){
+            return R.drawable.painter_palette_gray;
+        }else if(color == cColor(R.color.paint_black)){
+            return R.drawable.painter_palette_black;
+        }
+
+        return R.drawable.painter_palette_black;
+    }
 
 }
